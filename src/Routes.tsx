513f6659f8a2837893e0bd391d96594316f06bbd
choym0098 <choym0098@gmail.com>
2020-09-17 00:47:22 +0900

    Added main components and routes

---
+++ src/Routes.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import UserBoard from './components/UserBoard'
+
+
+export default function Routes() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign up</Link>
+            </li>
+            
+          </ul>
+        </nav>
+      </div>
+
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/login' exact component={Login} />
+        <Route path='/signup' exact component={Signup} />
+        <Route path='/board/:boardId' exact component={UserBoard} />
+      </Switch>
+    </Router>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./App";
+import Routes from "./Routes";
 ReactDOM.render(
-  <App />,
+  <Routes />,
   document.getElementById("output")
 );
